3169ee50787dbf5eee97fb45d92f5ac03434419f
swethambarikv <swethambarikv@gmail.com>
2022-07-11 09:20:20 +0530

    11-07-2022

--- index.js
+++ index.js
@@ -5,16 +5,21 @@ const adminRouter = require('./routes/admin-routes');
 const app = express();
 const cors=require('cors');
 const cookieParser = require('cookie-parser')
+require('dotenv').config()
 
 app.use(cors({origin:'http://localhost:4200'}));
 
 app.use(cookieParser());
-app.use(express.json());        
+app.use(express.json());   
 app.use('/users', userRouter);
 app.use('/admin',adminRouter);
 
 mongoose.connect("mongodb+srv://Swethambari:Swetha27gmail@cluster0.u2in2gx.mongodb.net/management?retryWrites=true&w=majority")
-    .then(() => console.log("Connnected to Database"))
+    .then(() => {
+        console.log("Connnected to Database")
+        console.log(process.env.DB_CONNECT)
+        console.log(process.env.SECRET_KEY)
+    })
     .then(() => {
         app.listen(8000);
     })
